d126bae4d1d6d58257fb17a1a4b319f214215c85
Khôi <nova1x1996@gmail.com>
2023-09-04 20:51:01 +0700

    ok

--- src/main/java/group2/client/controller/DonthuocController.java
+++ src/main/java/group2/client/controller/DonthuocController.java
@@ -1,9 +1,12 @@
 package group2.client.controller;
 
 
+import group2.client.dto.HoaDonThuocDAO;
+import group2.client.dto.ListHoaDonThuocDAO;
 import group2.client.entities.Donthuoc;
 import group2.client.entities.Thuoc;
 import group2.client.entities.Typethuoc;
+import java.util.ArrayList;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +17,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 @Controller
 @RequestMapping("/admin/donthuoc")
@@ -51,4 +57,36 @@ public class DonthuocController {
         model.addAttribute("donthuoc", new Donthuoc());
         return "admin/donthuoc/create";
     }
+    
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public String create(Model model,int[] thuocID,int[] price,int[] quantity) {
+            List<HoaDonThuocDAO> list = new ArrayList<HoaDonThuocDAO>();
+            for(int i = 0; i < thuocID.length;i++){
+                HoaDonThuocDAO obj = new HoaDonThuocDAO(thuocID[i],price[i],quantity[i]);
+                list.add(obj);
+            }
+            ListHoaDonThuocDAO listHDTD = new ListHoaDonThuocDAO();
+            listHDTD.setListHDT(list);
+            listHDTD.setName("Trần Minh Khôi");
+            
+                HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        // Tạo một HttpEntity với thông tin Casher để gửi yêu cầu POST
+        HttpEntity<ListHoaDonThuocDAO> request = new HttpEntity<>(listHDTD, headers);
+
+        ResponseEntity<ListHoaDonThuocDAO> response = restTemplate.exchange(apiUrl_Donthuoc, HttpMethod.POST, request, ListHoaDonThuocDAO.class);
+
+        // Kiểm tra mã trạng thái của phản hồi
+        if (response.getStatusCode().is2xxSuccessful()) {
+            // Thực hiện thêm xử lý sau khi tạo Casher thành công (nếu cần)
+
+            // Chuyển hướng về trang danh sách Casher
+            return "redirect:/admin/casher";
+        } else {
+            // Xử lý lỗi nếu cần thiết
+            return "/admin/casher/create";
+        }      
+    }
+    
 }
