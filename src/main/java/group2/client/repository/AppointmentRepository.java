c2dd2d02591aebb05e62bec8bcf684e2c8102bac
ThuongDeveloper <hoangdeptraibodoiqua4321@gmail.com>
2023-09-13 20:41:35 +0700

    no message

--- src/main/java/group2/client/repository/AppointmentRepository.java
+++ src/main/java/group2/client/repository/AppointmentRepository.java
@@ -5,6 +5,8 @@
 package group2.client.repository;
 
 import group2.client.entities.Appointment;
+import group2.client.entities.Doctor;
+import group2.client.entities.Patient;
 import java.util.Date;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,6 +16,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author hokim
  */
 public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
-    public List<Appointment> findByDate(Date date);
+    public List<Appointment> findByDate(Date date) ;
+            public List<Appointment> findByDateAndDoctorId(Date date,Doctor id);
+    public List<Appointment> findByDateAndPatientId(Date date,Patient id);
     public Appointment findByStarttimeAndEndtime(String starttime, String endtime);
 }
