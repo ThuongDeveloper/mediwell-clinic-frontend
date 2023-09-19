c49d2c8b02d31869836d867c77538ab64baf3c7c
dochitai <dochitai.xt123@gmail.com>
2023-09-19 14:32:26 +0700

    pdf

--- src/main/java/group2/client/controller/ToathuocController.java
+++ src/main/java/group2/client/controller/ToathuocController.java
@@ -3,16 +3,28 @@ package group2.client.controller;
 import group2.client.dto.HoaDonThuocDAO;
 import group2.client.dto.ListToaThuocDAO;
 import group2.client.dto.ToaThuocDAO;
+import group2.client.entities.Doctor;
 import group2.client.entities.Thuoc;
 import group2.client.entities.Toathuoc;
+import group2.client.service.AuthService;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,7 +33,8 @@ import java.util.List;
 public class ToathuocController {
     private String apiUrl_Toathuoc = "http://localhost:8888/api/toathuoc/";
     RestTemplate restTemplate = new RestTemplate();
-
+    @Autowired
+    private AuthService authService;
     @RequestMapping("")
     public String page(Model model) {
         ResponseEntity<List<Toathuoc>> response = restTemplate.exchange(apiUrl_Toathuoc, HttpMethod.GET, null,
@@ -53,7 +66,7 @@ public class ToathuocController {
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public String create(Model model,int[] thuocID,int[] quantity,int[] sang,int[] trua,int[] chieu,int[] toi,String name,String phone,String address, String sympton, String description,Boolean state) {
+    public String create(Model model, HttpServletRequest requestDoctor, int[] thuocID, int[] quantity, int[] sang, int[] trua, int[] chieu, int[] toi, String name, String phone, String address, String sympton, String description, Boolean state) {
         List<ToaThuocDAO> list = new ArrayList<ToaThuocDAO>();
         for(int i = 0; i < thuocID.length;i++){
             ToaThuocDAO obj = new ToaThuocDAO(thuocID[i],quantity[i],sang[i],trua[i],chieu[i],toi[i]);
@@ -62,6 +75,8 @@ public class ToathuocController {
         if (state == null) {
             state = false;
         }
+//        Doctor currentDoctor = authService.isAuthenticatedDoctor(requestDoctor);
+//        Integer doctorId = currentDoctor.getId();
         ListToaThuocDAO listTT = new ListToaThuocDAO();
         listTT.setListTT(list);
         listTT.setName(name);
@@ -70,6 +85,7 @@ public class ToathuocController {
         listTT.setDescription(description);
         listTT.setSymptom(sympton);
         listTT.setState(state);
+//        listTT.setDoctor_id(currentDoctor.getId());
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
@@ -88,5 +104,58 @@ public class ToathuocController {
             // Xử lý lỗi nếu cần thiết
             return "redirect:/admin/toathuoc";
         }
+    }
+
+    @RequestMapping(value = "/export-pdf", method = RequestMethod.GET)
+    public ResponseEntity<byte[]> exportToPDF(@RequestParam("toathuocId") int toathuocId) {
+        if (toathuocId <= 0) {
+            // Xử lý lỗi nếu tham số không hợp lệ
+            return ResponseEntity.badRequest().body("Invalid toathuocId".getBytes());
+        }
+
+        // Gọi API từ máy chủ backend để lấy tài liệu PDF
+        ResponseEntity<byte[]> response = restTemplate.exchange(apiUrl_Toathuoc + "/export-pdf?toathuocId=" + toathuocId, HttpMethod.GET, null, byte[].class);
+
+        // Kiểm tra mã trạng thái của phản hồi
+        if (response.getStatusCode().is2xxSuccessful()) {
+            byte[] pdfBytes = response.getBody();
+
+            // Thiết lập header và trả về file PDF
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_PDF);
+            headers.setContentDispositionFormData("attachment", "toathuoc.pdf");
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentLength(pdfBytes.length)
+                    .body(pdfBytes);
+        } else {
+            // Xử lý lỗi nếu không thể lấy tài liệu PDF từ API
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    private byte[] createToathuocPDF(Toathuoc toathuoc) throws IOException {
+        // Tạo một tài liệu PDF mới
+        PDDocument document = new PDDocument();
+
+        // Tạo một trang mới cho tài liệu
+        PDPage page = new PDPage();
+        document.addPage(page);
+
+        // Tạo một luồng nội dung cho trang
+        PDPageContentStream contentStream = new PDPageContentStream(document, page);
+
+        // Thực hiện viết nội dung PDF ở đây (ví dụ: ghi thông tin từ `toathuoc` vào tài liệu)
+
+        // Đóng luồng nội dung
+        contentStream.close();
+
+        // Lưu trang và đóng tài liệu
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        document.save(byteArrayOutputStream);
+        document.close();
+
+        return byteArrayOutputStream.toByteArray();
     }
 }
