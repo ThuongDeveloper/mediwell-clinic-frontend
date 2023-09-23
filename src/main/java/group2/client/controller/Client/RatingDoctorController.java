57a549ea5a338703443f869a89bd04d17e09a726
dochitai <dochitai.xt123@gmail.com>
2023-09-23 14:38:34 +0700

    validate rating

--- src/main/java/group2/client/controller/Client/RatingDoctorController.java
+++ src/main/java/group2/client/controller/Client/RatingDoctorController.java
@@ -2,6 +2,7 @@ package group2.client.controller.Client;
 
 import group2.client.dto.ListToaThuocDAO;
 import group2.client.dto.RatingDAO;
+import group2.client.entities.Appointment;
 import group2.client.entities.Doctor;
 import group2.client.entities.Patient;
 import group2.client.entities.Rating;
@@ -18,11 +19,15 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 public class RatingDoctorController {
     private String apiUrl = "http://localhost:8888/api/rating/";
+    String apiUrlAppointment = "http://localhost:8888/api/appointment/";
     private String apiUrlDoctor = "http://localhost:8888/api/doctor/";
     private String apiUrlPatient = "http://localhost:8888/api/patient";
     RestTemplate restTemplate = new RestTemplate();
@@ -39,34 +44,6 @@ public class RatingDoctorController {
     @Autowired
     private DoctorRepository doctorRepository;
 
-
-    @GetMapping("/rating/{patientID}")
-    public String editProfile(Model model, @PathVariable("patientID") Integer patientID, HttpServletRequest request) {
-
-        Patient currentPatient = authService.isAuthenticatedPatient(request);
-
-        ResponseEntity<Patient> response = restTemplate.exchange(apiUrlPatient + "/" + patientID, HttpMethod.GET, null,
-                new ParameterizedTypeReference<Patient>() {
-                });
-        if (response.getStatusCode().is2xxSuccessful() && currentPatient != null && currentPatient.getRole().equals("PATIENT")) {
-            Patient patient = response.getBody();
-            model.addAttribute("patientProfile", patient);
-            model.addAttribute("patient", currentPatient);
-        }
-
-        ResponseEntity<List<Doctor>> doctorResponse = restTemplate.exchange(apiUrlDoctor, HttpMethod.GET, null,
-                new ParameterizedTypeReference<List<Doctor>>() {
-                });
-
-        if (doctorResponse.getStatusCode().is2xxSuccessful()) {
-            List<Doctor> listDoctor = doctorResponse.getBody();
-            model.addAttribute("listDoctor", listDoctor);
-        }
-
-        model.addAttribute("rating", new Rating());
-
-        return "client/rating/index";
-    }
     private boolean containsProfanity(String comment) {
         // Thực hiện kiểm tra ở đây, ví dụ:
         String[] profanityWords = {"profanity1", "profanity2", "profanity3"};
@@ -77,6 +54,46 @@ public class RatingDoctorController {
         }
         return false;
     }
+    @GetMapping("/rating/{patientID}")
+    public String editProfile(Model model, @PathVariable("patientID") Integer patientID, HttpServletRequest request) {
+        Patient currentPatient = authService.isAuthenticatedPatient(request);
+
+        ResponseEntity<List<Appointment>> appointmentsResponse = restTemplate.exchange(apiUrlAppointment + "?patient_id=" + patientID, HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<Appointment>>() {});
+
+        if (appointmentsResponse.getStatusCode().is2xxSuccessful()) {
+            List<Appointment> appointments = appointmentsResponse.getBody();
+
+            // Tạo danh sách các bác sĩ đã có cuộc hẹn với bệnh nhân
+            List<Doctor> listDoctor = new ArrayList<>();
+
+            for (Appointment appointment : appointments) {
+                if (appointment.getPatientId().getId().equals(patientID)) {
+                    // Lấy đối tượng Doctor từ cuộc hẹn và thêm vào danh sách
+                    Doctor doctor = appointment.getDoctorId();
+                    listDoctor.add(doctor);
+                }
+            }
+
+            if (listDoctor.isEmpty()) {
+                // Nếu không có cuộc hẹn, thêm thông báo vào model
+                model.addAttribute("noAppointmentsMessage", "Không có cuộc hẹn nào cho bệnh nhân này.");
+            } else {
+                // Thêm danh sách bác sĩ vào model nếu có cuộc hẹn
+                model.addAttribute("listDoctor", listDoctor);
+            }
+        }
+
+        // Thêm thông tin bệnh nhân vào model
+        model.addAttribute("patientProfile", currentPatient);
+        model.addAttribute("patient", currentPatient);
+
+        // Thêm đối tượng Rating vào model để người dùng có thể đánh giá
+        model.addAttribute("rating", new Rating());
+
+        return "client/rating/index";
+    }
+
     @PostMapping("/rating/{patientId}")
     public String create(Model model, Double rating, String comment, int doctorId,
                          @PathVariable("patientId") Integer patientId, HttpServletRequest requestPatient) {
@@ -88,6 +105,11 @@ public class RatingDoctorController {
             // Xử lý lỗi nếu không có bệnh nhân đăng nhập
             model.addAttribute("error", "No authenticated patient.");
             return "client/rating/index";
+        }
+
+        // Kiểm tra nếu rating là null thì gán giá trị mặc định là 0
+        if (rating == null) {
+            rating = 0.0;
         }
 
         RatingDAO ratingDAO = new RatingDAO();
@@ -117,6 +139,7 @@ public class RatingDoctorController {
             return "client/rating/index";
         }
     }
+
 
 }
 
